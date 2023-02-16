d4b13567f01468cb666ac015ae4e9780fbef2372
Connor Ebanks <connorebanks@gmail.com>
2023-02-15 20:23:45 -0700

    First Commit

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Eric McCabe", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Connor Ebanks", "Fractions", "4.8", "7-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Colt Ford", "English Class", "The stuff about the stuff");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
